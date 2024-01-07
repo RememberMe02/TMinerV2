84cb273814e5613915d5013396f00f2485529d88
RememberMe02 <155920306+RememberMe02@users.noreply.github.com>
2024-01-07 22:25:40 +0100

    Create keep_alive.py

---
+++ keep_alive.py
@@ -0,0 +1,19 @@
+import os
+from flask import Flask
+from threading import Thread
+
+app = Flask('')
+
+
+@app.route('/')
+def main():
+  return os.environ.get("user") + " is alive."
+
+
+def run():
+  app.run(host="0.0.0.0", port=8080)
+
+
+def keep_alive():
+  server = Thread(target=run)
+  server.start()
